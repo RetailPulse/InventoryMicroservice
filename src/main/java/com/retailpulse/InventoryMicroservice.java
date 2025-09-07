28d525b480d98115cfdd5f0ee68dc768e6c35872
Galvin <galvinleow@gmail.com>
2025-09-07 22:07:15 +0800

    Adding Cache

--- src/main/java/com/retailpulse/InventoryMicroservice.java
+++ src/main/java/com/retailpulse/InventoryMicroservice.java
@@ -2,9 +2,11 @@ package com.retailpulse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableCaching
 @EnableFeignClients(basePackages = "com.retailpulse.client")
 public class InventoryMicroservice {
     public static void main(String[] args) {
